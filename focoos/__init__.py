a8aaec7ec224c7f2cd44430c5bd7060ead96b74b
Giuseppe Ambrosio <giuambro97@gmail.com>
2025-01-03 09:49:25 +0100

    refactor(Focoos): reorganize imports and update __all__ for better module structure

--- focoos/__init__.py
+++ focoos/__init__.py
@@ -1,5 +1,54 @@
+from .config import FOCOOS_CONFIG
 from .focoos import Focoos
 from .local_model import LocalModel
-from .ports import *
+from .ports import (
+    DatasetInfo,
+    DatasetLayout,
+    DatasetMetadata,
+    DeploymentMode,
+    FocoosDet,
+    FocoosDetections,
+    FocoosTask,
+    GPUInfo,
+    Hyperparameters,
+    LatencyMetrics,
+    ModelMetadata,
+    ModelPreview,
+    ModelStatus,
+    OnnxEngineOpts,
+    RuntimeTypes,
+    SystemInfo,
+    TraininingInfo,
+    TrainInstance,
+)
 from .remote_model import RemoteModel
+from .runtime import ONNXRuntime, get_runtime
 from .utils.system import get_system_info
+
+__all__ = [
+    "FOCOOS_CONFIG",
+    "Focoos",
+    "LocalModel",
+    "RemoteModel",
+    "FocoosDetections",
+    "FocoosDet",
+    "FocoosTask",
+    "ModelMetadata",
+    "ModelStatus",
+    "DatasetInfo",
+    "DatasetLayout",
+    "DatasetMetadata",
+    "DeploymentMode",
+    "GPUInfo",
+    "Hyperparameters",
+    "LatencyMetrics",
+    "ModelPreview",
+    "OnnxEngineOpts",
+    "RuntimeTypes",
+    "SystemInfo",
+    "TraininingInfo",
+    "TrainInstance",
+    "get_system_info",
+    "ONNXRuntime",
+    "get_runtime",
+]
