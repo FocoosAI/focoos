49a6d8741f639ff16834695434de3e1d512026d6
fcdl94 <f.cermelli94@gmail.com>
2025-07-18 09:37:11 +0000

    refactor: simplify Keypoints constructor by removing numpy array support

--- focoos/structures.py
+++ focoos/structures.py
@@ -816,14 +816,17 @@ class Keypoints:
     * v=2: labeled and visible
     """
 
-    def __init__(self, keypoints: Union[torch.Tensor, np.ndarray, List[List[float]]]):
+    def __init__(self, keypoints: Union[torch.Tensor, List[List[float]]]):
         """
         Arguments:
-            keypoints: A Tensor, numpy array, or list of the x, y, and visibility of each keypoint.
+            keypoints: A Tensor or list of the x, y, and visibility of each keypoint.
                 The shape should be (N, K, 3) where N is the number of
                 instances, and K is the number of keypoints per instance.
         """
+        if isinstance(keypoints, list):
+            keypoints = np.array(keypoints)
         device = keypoints.device if isinstance(keypoints, torch.Tensor) else torch.device("cpu")
+
         keypoints = torch.as_tensor(keypoints, dtype=torch.float32, device=device)
         assert keypoints.dim() == 3 and keypoints.shape[2] == 3, keypoints.shape
         self.tensor = keypoints
